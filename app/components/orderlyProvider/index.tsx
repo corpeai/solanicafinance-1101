3efc7effb909cc8536e68007a2518e88cdc39f1c
CorpAI <35112540+corpeai@users.noreply.github.com>
2026-06-14 21:56:58 -0400

    Update index.tsx

--- app/components/orderlyProvider/index.tsx
+++ app/components/orderlyProvider/index.tsx
@@ -16,12 +16,6 @@ import { registerPlugin } from "@orderly.network/orderbook-shimmer-plugin";
 import { registerFastPlaceOrderPlugin } from "@orderly.network/fast-place-order-plugin";
 
 
-const plugins = [
-  FastPlaceOrderPlugin({
-    autoShowOnFullscreen: false, // optional, default: true
-  }),
-];
-
 const NETWORK_ID_KEY = "orderly_network_id";
 
 const getNetworkId = (): NetworkId => {
